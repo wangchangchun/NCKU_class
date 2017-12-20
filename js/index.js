8ef84b0341ce14f46960a1e6dfe96f7ce771ee53
wangchangchun <j432985029@gmail.com>
2017-12-20 18:07:41 +0800

    OAO

--- js/index.js
+++ js/index.js
@@ -32,7 +32,7 @@
 
   window.fbAsyncInit = function() {
   FB.init({
-    appId      : '257340648129250',
+    appId      : '1071353126339792',
     cookie     : true,  // enable cookies to allow the server to access 
                         // the session
     xfbml      : true,  // parse social plugins on this page
